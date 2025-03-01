f65c7ad3ca3a709fd760f6102cb58d597f920d29
lurkydismal <lurkydismal@duck.com>
2025-03-01 11:45:35 +0500

    1

---
+++ build.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+export SCRIPT_DIRECTORY=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+export BUILD_DIRECTORY_NAME='out'
+export BUILD_DIRECTORY="$SCRIPT_DIRECTORY/$BUILD_DIRECTORY_NAME"
+
+# 0 - Debug
+# 1 - Release
+# 2 - Profile
+export BUILD_TYPE=0
+
+export BUILD_C_FLAGS="-flto=jobserver -std=gnu99 -march=native -ffunction-sections -fdata-sections -fPIC -fopenmp-simd -fno-ident -fshort-enums -Wall -Wextra"
+export BUILD_C_FLAGS_DEBUG="-Og -g"
+export BUILD_C_FLAGS_RELEASE="-Ofast -funroll-loops -fno-asynchronous-unwind-tables"
+export BUILD_C_FLAGS_PROFILE="$BUILD_C_FLAGS_RELEASE"
+
+export declare BUILD_DEFINES=(
+)
+
+export declare BUILD_DEFINES_DEBUG=(
+    "DEBUG"
+)
+
+export declare BUILD_DEFINES_RELEASE=(
+    "RELEASE"
+)
+
+export declare BUILD_DEFINES_PROFILE=(
+    "PROFILE"
+)
+
+export declare BUILD_INCLUDES=(
+    "glad/include"
+)
+
+export LINK_FLAGS="-flto -fPIC -fuse-ld=mold -Wl,-O1 -Wl,--gc-sections -Wl,--no-eh-frame-hdr"
+export LINK_FLAGS_DEBUG="-g"
+export LINK_FLAGS_RELEASE="-s"
+export LINK_FLAGS_PROFILE="$LINK_FLAGS_DEBUG"
+
+export declare LIBRARIES_TO_LINK=(
+    "glfw"
+    "dl"
+    "mimalloc"
+)
+export LINKER="ccache gcc"
+export EXECUTABLE_NAME="main.out"
+export declare EXECUTABLE_SECTIONS_TO_STRIP=(
+    ".note.gnu.build-id"
+    ".note.gnu.property"
+    ".comment"
+    ".eh_frame"
+    ".eh_frame_hdr"
+    ".relro_padding"
+)
+
+clear
+
+source './config.sh' && {
+
+mkdir -p "$BUILD_DIRECTORY"
+
+if [ $BUILD_TYPE -eq 0 ]; then
+    echo "Debug build"
+
+    BUILD_C_FLAGS="$BUILD_C_FLAGS $BUILD_C_FLAGS_DEBUG"
+    LINK_FLAGS="$LINK_FLAGS $LINK_FLAGS_DEBUG"
+    BUILD_DEFINES+=( "${BUILD_DEFINES_DEBUG[@]}" )
+
+elif [ $BUILD_TYPE -eq 1 ]; then
+    echo "Release build"
+
+    BUILD_C_FLAGS="$BUILD_C_FLAGS $BUILD_C_FLAGS_RELEASE"
+    LINK_FLAGS="$LINK_FLAGS $LINK_FLAGS_RELEASE"
+    BUILD_DEFINES+=( "${BUILD_DEFINES_RELEASE[@]}" )
+
+elif [ $BUILD_TYPE -eq 2 ]; then
+    echo "Profile build"
+
+    BUILD_C_FLAGS="$BUILD_C_FLAGS $BUILD_C_FLAGS_PROFILE"
+    LINK_FLAGS="$LINK_FLAGS $LINK_FLAGS_PROFILE"
+    BUILD_DEFINES+=( "${BUILD_DEFINES_PROFILE[@]}" )
+fi
+
+for partToBuild in "${partsToBuild[@]}"; do
+    if [ ${#BUILD_DEFINES[@]} -ne 0 ]; then
+        printf -v definesAsString -- "-D %s " "${BUILD_DEFINES[@]}"
+        echo $definesAsString
+    fi
+
+    if [ ${#BUILD_INCLUDES[@]} -ne 0 ]; then
+        printf -v includesAsString -- "-I $SCRIPT_DIRECTORY/%s " "${BUILD_INCLUDES[@]}"
+        echo $includesAsString
+    fi
+
+    source "$partToBuild/config.sh" && './build_general.sh' "$partToBuild" "$BUILD_C_FLAGS" "$definesAsString" "$includesAsString"
+
+    BUILD_STATUS=$?
+
+    if [ $BUILD_STATUS -ne 0 ]; then
+        break
+    fi
+done
+
+if [ $BUILD_STATUS -eq 0 ]; then
+    if [ ${#partsToBuild[@]} -ne 0 ]; then
+        printf -v partsToBuildAsString -- "$BUILD_DIRECTORY/lib%s.a " "${partsToBuild[@]}"
+        echo $partsToBuildAsString
+    fi
+
+    if [ ${#LIBRARIES_TO_LINK[@]} -ne 0 ]; then
+        printf -v librariesToLinkAgainst -- "-l%s " "${LIBRARIES_TO_LINK[@]}"
+        echo $librariesToLinkAgainst
+    fi
+
+    $LINKER $LINK_FLAGS $partsToBuildAsString $librariesToLinkAgainst -o "$BUILD_DIRECTORY/$EXECUTABLE_NAME"
+
+    if [ ! -z "${NEED_STRIP_EXECUTABLE+x}" ]; then
+        if [ ${#EXECUTABLE_SECTIONS_TO_STRIP[@]} -ne 0 ]; then
+            printf -v sectionsToStripAsString -- "--remove-section %s " "${EXECUTABLE_SECTIONS_TO_STRIP[@]}"
+            echo $sectionsToStripAsString
+        fi
+
+        objcopy "$BUILD_DIRECTORY/$EXECUTABLE_NAME" $sectionsToStripAsString
+
+        strip --strip-section-headers "$BUILD_DIRECTORY/$EXECUTABLE_NAME"
+    fi
+fi
+
+}
---
+++ main/src/main.c
@@ -0,0 +1,157 @@
+#define GLFW_INCLUDE_NONE
+#define GLAD_GL_IMPLEMENTATION
+
+#include <GLFW/glfw3.h>
+#include <glad/gl.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+#define ONE_MILLISECOND_IN_NANOSECONDS 1000000
+#define MILLISECONDS_TO_NANOSECONDS( _milliseconds ) \
+    ( _milliseconds * ONE_MILLISECOND_IN_NANOSECONDS )
+
+static size_t g_fps = 0;
+static bool g_exit = false;
+
+void error_callback( int error, const char* description ) {
+    fprintf( stderr, "Error: %d %s\n", error, description );
+}
+
+void* t( void* data ) {
+    static size_t l_fps = 0;
+
+    ( void )( sizeof( data ) );
+
+    while ( !g_exit ) {
+        printf( "%lu\n", ( g_fps - l_fps ) );
+
+        l_fps = g_fps;
+
+        sleep( 1 );
+    }
+
+    return ( NULL );
+}
+
+static void keyCallback( GLFWwindow* window,
+                          int key,
+                          int scancode,
+                          int action,
+                          int mods ) {
+    l_callbackResult = event( key, mods );
+
+    if ( UNLIKELY( !( l_callbackResult == ( callbackResult_t )remain ) ) ) {
+        glfwSetWindowShouldClose( window, GLFW_TRUE );
+    }
+}
+
+int main( void ) {
+    glfwSetErrorCallback( error_callback );
+
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if ( !glfwInit() )
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow( 640, 480, "Hello World", NULL, NULL );
+    if ( !window ) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwSetKeyCallback( window, key_callback );
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent( window );
+
+    int version = gladLoadGL( glfwGetProcAddress );
+    printf( "GL %d.%d\n", GLAD_VERSION_MAJOR( version ),
+            GLAD_VERSION_MINOR( version ) );
+
+    glfwSwapInterval( 1 );
+
+    struct timespec next_frame;
+
+    next_frame.tv_sec = 0;
+    next_frame.tv_nsec = MILLISECONDS_TO_NANOSECONDS( 16.6666667 );
+
+#if 1
+    pthread_t tr;
+
+    if ( pthread_create( &tr, NULL, t, NULL ) ) {
+        printf( "error: \n" );
+    }
+#endif
+
+    for ( ;; ) {
+        {
+            /* Poll for and process events */
+            glfwPollEvents();
+        }
+
+        {
+            int width, height;
+            glfwGetFramebufferSize( window, &width, &height );
+            glViewport( 0, 0, width, height );
+
+            /* Render here */
+            glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+
+            /* Swap front and back buffers */
+            glfwSwapBuffers( window );
+        }
+
+        {
+            g_fps++;
+
+#if 0
+            clock_nanosleep( CLOCK_MONOTONIC, 0, &next_frame, NULL );
+#endif
+        }
+
+        if ( glfwWindowShouldClose( window ) ) {
+            g_exit = true;
+
+            break;
+        }
+    }
+
+#if 1
+    pthread_join( tr, NULL );
+#endif
+
+    glfwTerminate();
+
+    return ( 0 );
+}
+
+int main( void ) {
+    callbackResult_t l_callbackResult = ( callbackResult_t )success;
+
+    l_callbackResult = init();
+
+    if ( UNLIKELY( !( l_callbackResult == ( callbackResult_t )remain ) ) ) {
+        goto EXIT;
+    }
+
+    glfwSetKeyCallback( window, key_callback );
+
+    // Not limited iteration
+    while ( !glfwWindowShouldClose( window ) ) {
+        l_callbackResult = iterate();
+
+        if ( UNLIKELY(
+                    !( l_callbackResult == ( callbackResult_t )remain ) ) ) {
+            break;
+        }
+    }
+
+EXIT:
+    return ( quit( l_callbackResult ) );
+}
