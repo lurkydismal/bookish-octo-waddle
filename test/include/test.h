67c0e6b2db30b75264f915b3402f6fb7c59856ad
lurkydismal <lurkydismal@duck.com>
2025-03-12 16:45:46 +0000

    1

--- test/include/test.h
+++ test/include/test.h
@@ -2,32 +2,38 @@
 
 #include <stdio.h>
 
+#define RED "\e[1;31m"
+#define GREEN "\e[1;32m"
+#define YELLOW "\e[1;33m"
+#define RESET "\e[0m"
+
 #define MAX_TESTS 100
 
-typedef int ( *test_func )( void );
+typedef int ( *testFunc_t )( void );
 
 typedef struct {
     const char* name;
-    test_func func;
-} test_entry;
+    testFunc_t func;
+} testEntry_t;
 
-extern test_entry test_registry[ MAX_TESTS ];
-extern int test_count;
+extern testEntry_t g_testRegistry[ MAX_TESTS ];
+extern size_t g_testCount;
 
-#define TEST( _name )                                                        \
-    static int _name( void ) __attribute__( ( used ) );                      \
-    __attribute__( ( constructor ) ) static void register_##_name( void ) {  \
-        if ( test_count < MAX_TESTS ) {                                      \
-            test_registry[ test_count++ ] = ( test_entry ){ #_name, _name }; \
-        }                                                                    \
-    }                                                                        \
+#define TEST( _name )                                                       \
+    static int _name( void ) __attribute__( ( used ) );                     \
+    __attribute__( ( constructor ) ) static void register_##_name( void ) { \
+        if ( g_testCount < MAX_TESTS ) {                                    \
+            g_testRegistry[ g_testCount++ ] =                               \
+                ( testEntry_t ){ #_name, _name };                           \
+        }                                                                   \
+    }                                                                       \
     static int _name( void )
 
-#define ASSERT_EQ( _actual, _expected )                                   \
-    do {                                                                  \
-        if ( ( _actual ) != ( _expected ) ) {                             \
-            printf( "[FAILED] %s:%d: Expected %d but got %d\n", __FILE__, \
-                    __LINE__, ( _expected ), ( _actual ) );               \
-            return ( 1 );                                                 \
-        }                                                                 \
+#define ASSERT_EQ( _actual, _expected )                                      \
+    do {                                                                     \
+        if ( ( _actual ) != ( _expected ) ) {                                \
+            printf( RED "[FAILED]" RESET " %s:%d: Expected %d but got %d\n", \
+                    __FILE__, __LINE__, ( _expected ), ( _actual ) );        \
+            return ( 1 );                                                    \
+        }                                                                    \
     } while ( 0 )
