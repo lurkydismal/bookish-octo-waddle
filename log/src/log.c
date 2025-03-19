ad464ba7e7ef24c98e6c1adcac119d4639d820d0
lurkydismal <lurkydismal@duck.com>
2025-03-19 05:07:52 +0000

    Enhance logging module with transaction handling, dynamic log levels, and color support
    - Introduced log level management with enumeration for debug, info, warn, error, and unknown log levels.
    - Added support for setting and querying log levels via both enums and strings.
    - Implemented transaction-based logging with configurable maximum transaction size.
    - Added color-coded log output for better readability, with different colors for each log level.
    - Improved log file handling with dynamic file paths and proper file descriptor management.
    - Refactored log$transaction$query to include log level checks before logging a message.
    - Updated log$transaction$commit to handle both file and stdout logging with color formatting.
    - Optimized memory allocation for transaction strings and log buffers.
    
    Files affected:
    - log/include/log.h: Added log level enums, new functions for log level handling and transaction logging.
    - log/src/log.c: Implemented transaction-based logging, log level management, and output enhancements.
    - stdfunc/include/stdfunc.h: Removed redundant file descriptor definitions for STDIN, STDOUT, STDERR.

--- log/src/log.c
+++ log/src/log.c
@@ -1,34 +1,220 @@
 #include "log.h"
 
+#include <fcntl.h>
 #include <mimalloc.h>
-#include <stddef.h>
 #include <unistd.h>
 
 #include "stdfunc.h"
 
-#define LOG_FILE_NAME "log"
-#define LOG_FILE_EXTENSION "txt"
-#define MAX_TRANSACITON_STRING_SIZE ( 1024 * 2 )
+static int g_fileDescriptor;
+static char* g_transactionString;
+static ssize_t g_transactionSize = 0;
+static size_t g_maxTransactionSize;
+static logLevel_t g_currentLogLevel = LOG_LEVEL_DEFAULT;
 
-static int g_fileDescriptor = 1;
-static char g_transactionString[ MAX_TRANSACITON_STRING_SIZE ];
-static ssize_t g_transactionStringLength = 0;
+static const char* log$level$convert$toString( logLevel_t _logLevel ) {
+    switch ( _logLevel ) {
+        case ( logLevel_t )debug: {
+            return ( LOG_LEVEL_AS_STRING_DEBUG );
+        }
 
-bool log$transaction$query( const char* _string ) {
+        case ( logLevel_t )info: {
+            return ( LOG_LEVEL_AS_STRING_INFO );
+        }
+
+        case ( logLevel_t )warn: {
+            return ( LOG_LEVEL_AS_STRING_WARN );
+        }
+
+        case ( logLevel_t )error: {
+            return ( LOG_LEVEL_AS_STRING_ERROR );
+        }
+
+        default: {
+            return ( LOG_LEVEL_AS_STRING_UNKNOWN );
+        }
+    }
+}
+
+static logLevel_t log$level$convert$fromString( const char* _string ) {
+    if ( __builtin_strcmp( _string, LOG_LEVEL_AS_STRING_DEBUG ) == 0 ) {
+        return ( ( logLevel_t )debug );
+
+    } else if ( __builtin_strcmp( _string, LOG_LEVEL_AS_STRING_INFO ) == 0 ) {
+        return ( ( logLevel_t )info );
+
+    } else if ( __builtin_strcmp( _string, LOG_LEVEL_AS_STRING_WARN ) == 0 ) {
+        return ( ( logLevel_t )warn );
+
+    } else if ( __builtin_strcmp( _string, LOG_LEVEL_AS_STRING_ERROR ) == 0 ) {
+        return ( ( logLevel_t )error );
+
+    } else {
+        return ( ( logLevel_t )unknown );
+    }
+}
+
+static const char* log$level$convert$toColor( logLevel_t _logLevel ) {
+    switch ( _logLevel ) {
+        case ( logLevel_t )debug: {
+            return ( LOG_COLOR_DEBUG );
+        }
+
+        case ( logLevel_t )info: {
+            return ( LOG_COLOR_INFO );
+        }
+
+        case ( logLevel_t )warn: {
+            return ( LOG_COLOR_WARN );
+        }
+
+        case ( logLevel_t )error: {
+            return ( LOG_COLOR_ERROR );
+        }
+
+        default: {
+            return ( LOG_COLOR_UNKNOWN );
+        }
+    }
+}
+
+bool log$init( const char* _fileName,
+               const char* _fileExtension,
+               const size_t _maxTransactionSize ) {
     bool l_returnValue = false;
 
-    const ssize_t l_stringLength = __builtin_strlen( _string );
+    {
+        // Open file descriptor
+        {
+            const size_t l_fileNameLength = __builtin_strlen( _fileName );
+            const size_t l_fileExtensionLength =
+                __builtin_strlen( _fileExtension );
+            const size_t l_filePathLength =
+                ( l_fileNameLength + 1 + l_fileExtensionLength );
 
-    if ( ( g_transactionStringLength + l_stringLength ) >
-         MAX_TRANSACITON_STRING_SIZE ) {
+            {
+                // +1 for file name and extension separator
+                char* l_filePath = ( char* )mi_malloc(
+                    ( l_filePathLength + 1 ) * sizeof( char ) );
+
+                {
+                    size_t l_index = 0;
+
+                    __builtin_memcpy( ( l_filePath + l_index ), _fileName,
+                                      l_fileNameLength );
+                    l_index += l_fileNameLength;
+
+                    // Separator
+                    l_filePath[ l_index ] = '.';
+                    l_index += 1;
+
+                    __builtin_memcpy( ( l_filePath + l_index ), _fileExtension,
+                                      l_fileExtensionLength );
+                    l_index += l_fileExtensionLength;
+
+                    l_filePath[ l_filePathLength ] = '\0';
+                }
+
+                g_fileDescriptor = open( l_filePath, O_WRONLY | O_CREAT, 0644 );
+
+                mi_free( l_filePath );
+            }
+
+            if ( g_fileDescriptor == -1 ) {
+                goto EXIT;
+            }
+        }
+
+        // Set max transaction size
+        {
+            g_maxTransactionSize = _maxTransactionSize;
+
+            if ( !g_maxTransactionSize ) {
+                goto EXIT;
+            }
+        }
+
+        // Allocate transaction string
+        {
+            g_transactionString =
+                ( char* )mi_malloc( g_maxTransactionSize * sizeof( char ) );
+        }
+
+        l_returnValue = true;
+    }
+
+EXIT:
+    return ( l_returnValue );
+}
+
+bool log$quit( void ) {
+    bool l_returnValue = false;
+
+    {
+        if ( close( g_fileDescriptor ) == -1 ) {
+            goto EXIT;
+        }
+
+        mi_free( g_transactionString );
+
+        l_returnValue = true;
+    }
+
+EXIT:
+    return ( l_returnValue );
+}
+
+bool log$level$set( const logLevel_t _logLevel ) {
+    bool l_returnValue = false;
+
+    {
+        g_currentLogLevel = _logLevel;
+
+        l_returnValue = true;
+    }
+
+    return ( l_returnValue );
+}
+
+bool log$level$set$string( const char* _string ) {
+    bool l_returnValue = false;
+
+    {
+        g_currentLogLevel = log$level$convert$fromString( _string );
+
+        l_returnValue = true;
+    }
+
+    return ( l_returnValue );
+}
+
+logLevel_t log$level$get( void ) {
+    return ( g_currentLogLevel );
+}
+
+const char* log$level$get$string( void ) {
+    return ( log$level$convert$toString( g_currentLogLevel ) );
+}
+
+bool log$transaction$query( const logLevel_t _logLevel, const char* _string ) {
+    bool l_returnValue = false;
+
+    if ( _logLevel < g_currentLogLevel ) {
+        goto EXIT;
+    }
+
+    const size_t l_stringLength = __builtin_strlen( _string );
+
+    if ( UNLIKELY( ( g_transactionSize + l_stringLength ) >
+                   g_maxTransactionSize ) ) {
         goto EXIT;
     }
 
     {
-        __builtin_memcpy( ( g_transactionString + g_transactionStringLength ),
-                          _string, l_stringLength );
+        __builtin_memcpy( ( g_transactionString + g_transactionSize ), _string,
+                          l_stringLength );
 
-        g_transactionStringLength += l_stringLength;
+        g_transactionSize += l_stringLength;
 
         l_returnValue = true;
     }
@@ -46,14 +232,13 @@ EXIT:
 bool log$transaction$commit( void ) {
     bool l_returnValue = false;
 
-    if ( g_transactionStringLength ) {
+    if ( g_transactionSize ) {
         // Log to file descriptor
         {
-            const ssize_t l_writtenCount =
-                write( g_fileDescriptor, g_transactionString,
-                       g_transactionStringLength );
+            const ssize_t l_writtenCount = write(
+                g_fileDescriptor, g_transactionString, g_transactionSize );
 
-            l_returnValue = ( l_writtenCount == g_transactionStringLength );
+            l_returnValue = ( l_writtenCount == g_transactionSize );
 
             if ( !l_returnValue ) {
                 goto EXIT;
@@ -65,39 +250,63 @@ bool log$transaction$commit( void ) {
         // Log to stdout
 #if defined( DEBUG )
         {
-            const char l_logSignature[] = { 'L', 'O', 'G', ':' };
+            static const char l_logSignature[] = { 'L', 'O', 'G', ':' };
 
+            // 1 for [
+            // 1 for ]
+            // 1 for space
+            // 1 for new line
             const ssize_t l_bufferLength =
-                ( arrayLengthNative( l_logSignature ) +
-                  g_transactionStringLength );
+                ( LOG_COLOR_MAX_LENGTH + 1 + LOG_LEVEL_AS_STRING_MAX_LENGTH +
+                  1 + __builtin_strlen( COLOR_RESET ) * sizeof( char ) + 1 +
+                  arrayLengthNative( l_logSignature ) + g_transactionSize + 1 );
 
+            // TODO: Limit scope for allocated variable
             char* l_buffer =
                 ( char* )mi_malloc( l_bufferLength * sizeof( char ) );
+
+            {
+                const char* l_logLevelColor =
+                    log$level$convert$toColor( g_currentLogLevel );
+                const char* l_logLevelAsString =
+                    log$level$convert$toString( g_currentLogLevel );
+
+                __builtin_snprintf( l_buffer, l_bufferLength, "%s[%s]%s %s\n",
+                                    l_logLevelColor, l_logLevelAsString,
+                                    COLOR_RESET, g_transactionString );
+            }
 
             __builtin_memcpy( l_buffer, l_logSignature,
                               arrayLengthNative( l_logSignature ) );
             __builtin_memcpy(
                 ( l_buffer + arrayLengthNative( l_logSignature ) ),
-                g_transactionString, g_transactionStringLength );
+                g_transactionString, g_transactionSize );
 
             const ssize_t l_writtenCount =
-                write( STDOUT, l_buffer, l_bufferLength );
+                write( STDOUT_FILENO, l_buffer, __builtin_strlen( l_buffer ) );
 
             mi_free( l_buffer );
 
-            l_returnValue = ( l_writtenCount == g_transactionStringLength );
+            if ( !l_writtenCount ) {
+                l_returnValue = false;
+
+                goto EXIT;
+            }
+
+            l_returnValue =
+                ( ( size_t )l_writtenCount == __builtin_strlen( l_buffer ) );
 
             if ( !l_returnValue ) {
                 goto EXIT;
             }
 
-            fdatasync( STDOUT );
+            fdatasync( STDOUT_FILENO );
         }
 #endif
     }
 
 EXIT:
-    g_transactionStringLength = 0;
+    g_transactionSize = 0;
 
     return ( l_returnValue );
 }
